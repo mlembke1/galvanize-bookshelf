063bd6e637113ac5ba8aae063fe6ad1f707a8c99
Mitch Lembke <mitchell.lembke@gmail.com>
2018-04-02 16:04:00 -0600

    wrote route for /books

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,21 @@
-'use strict';
-
+// 'use strict';
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const knex = require('../knex')
+const humps =  require('humps')
 
 // YOUR CODE HERE
+router.use(bodyParser.json())
+
+router.get('/books', (req, res) => {
+  knex('books')
+  .orderBy('title', 'asc')
+  .then(book => {
+
+    res.json(humps.camelizeKeys(book))
+  })
+})
 
 module.exports = router;
