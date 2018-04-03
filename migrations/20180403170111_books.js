5ee406b280b49fa4d316b2b99e4fda31fa369adf
Mitch Lembke <mitchell.lembke@gmail.com>
2018-04-03 17:21:10 -0600

    burned migration and seeds. built new migration

---
+++ migrations/20180403170111_books.js
@@ -0,0 +1,16 @@
+exports.up = (knex, Promise) => {
+    return knex.schema.createTable('books', (table) => {
+     // TABLE COLUMN DEFINITIONS HERE
+     table.increments('id').primary()
+     table.string('title', 255).notNullable().defaultTo('')
+     table.string('author', 255).notNullable().defaultTo('')
+     table.string('genre', 255).notNullable().defaultTo('')
+     table.text('description', 255).notNullable().defaultTo('')
+     table.text('cover_url', 255).notNullable().defaultTo('')
+     table.dateTime('created_at').notNullable().defaultTo(knex.raw('now()'))
+     table.dateTime('updated_at').notNullable().defaultTo(knex.raw('now()'))
+   })
+}
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTableIfExists('tablename')
+}
--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,53 @@
 // 'use strict';
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const knex = require('../knex')
 const humps =  require('humps')
 
 // YOUR CODE HERE
-router.use(bodyParser.json())
+// router.use(bodyParser.json())
+
+let lastId  
 
 router.get('/books', (req, res) => {
   knex('books')
   .orderBy('title', 'asc')
   .then(book => {
-
+    lastId = book[book.length-1].id
     res.json(humps.camelizeKeys(book))
+  })
+})
+
+router.get('/books/:id', (req, res) => {
+  const { id } = req.params
+  knex('books')
+  .where('id', id)
+  .then(book => {
+    res.json(humps.camelizeKeys(book)[0])
+  })
+})
+
+router.post('/books', (req, res) => {
+    req.body['id'] = lastId + 1
+    res.status(200).json(humps.camelizeKeys(req.body))
+  })
+
+
+router.patch('/books/:id', (req, res) => {
+  const { id } = req.params
+  req.body['id'] = id
+  res.status(200).json(req.body)
+})
+
+router.delete('/books/:id', (req, res) => {
+  const { id } = req.params
+  knex('books')
+  .select('title', 'author', 'genre', 'description', 'cover_url')
+  .where('id', id)
+  .then(book => {
+    res.status(200).json(humps.camelizeKeys(book)[0])
   })
 })
 
