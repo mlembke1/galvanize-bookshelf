218fd3e4c48b6b09e2c9d1d2ece075e18cc9c737
Mitch Lembke <mitchell.lembke@gmail.com>
2018-04-04 14:51:55 -0600

    Hashed password enabled upon user post request

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,27 @@
 'use strict';
-
 const express = require('express');
-
-// eslint-disable-next-line new-cap
+const knex = require('../knex')
+const humps =  require('humps')
 const router = express.Router();
+const bcrypt = require('bcrypt')
 
 // YOUR CODE HERE
+router.post('/', (req, res, next) => {
+  bcrypt.genSalt(8, (err, salt) => {
+    bcrypt.hash(req.body.password, salt, (err, hashed) => {
+      knex('users')
+      .insert({
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
+        email: req.body.email,
+        hashed_password: hashed
+      })
+      .returning(['first_name', 'last_name', 'email', 'id'])
+      .then(result => {
+        res.status(200).json(humps.camelizeKeys(result[0]))
+      })
+    })
+  })
+})
 
 module.exports = router;
